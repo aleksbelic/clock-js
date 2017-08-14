dec35e7f7aa93d6d1183e8ccde1e5d48a30cd3e5
Aleksandar Belić <Aleksandar.Belic@thinkpadedge-29.netcare.local>
2017-08-14 10:52:48 +0200

    Seconds hand is now dynamic, clock face is larger.

--- clockJS.js
+++ clockJS.js
@@ -1,6 +1,8 @@
-var CYRCLE_CENTER_X = 75;
-var CYRCLE_CENTER_Y = 75;
-var CYRCLE_RADIUS = 50;
+var CANVAS_WIDTH = 300;
+var CANVAS_HEIGHT = 300;
+var CYRCLE_CENTER_X = 150;
+var CYRCLE_CENTER_Y = 150;
+var CYRCLE_RADIUS = 100;
 var hourHandLengthProcent = 100;
 var hourHandLength = CYRCLE_RADIUS * hourHandLengthProcent / 100;
 var hourHandX;
@@ -8,11 +10,10 @@ var hourHandXLength;
 var hourHandY;
 var hourHandYLength;
 
-var d = new Date();
-var hour = d.getHours();
+var hour = new Date().getHours();
 //console.log("hour: " + hour);
-var min = d.getMinutes();
-console.log("min: " + min);
+var min = new Date().getMinutes();
+//console.log("min: " + min);
 
 function getHourHandYLength(hourHandXLength) {
 	return Math.sqrt(hourHandLength * hourHandLength - hourHandXLength * hourHandXLength);
@@ -20,58 +21,14 @@ function getHourHandYLength(hourHandXLength) {
 
 document.addEventListener("DOMContentLoaded", function() {
 	var canvas = document.getElementById('clockJS-canvas');
+	canvas.setAttribute("width", CANVAS_WIDTH);
+	canvas.setAttribute("height", CANVAS_HEIGHT);
 	if (canvas.getContext) {
 		var ctx = canvas.getContext('2d');
-		ctx.beginPath();
-		ctx.strokeStyle = '#000000';
-		// arc(x, y, radius, startAngle, endAngle, anticlockwise)
-		ctx.arc(CYRCLE_CENTER_X, CYRCLE_CENTER_Y, CYRCLE_RADIUS, 0, 2 * Math.PI);
-		ctx.stroke();
-		
-		// hour hand
-		/*
-		ctx.beginPath();
-		ctx.strokeStyle = 'red';
-		ctx.lineWidth= '2';
-		ctx.moveTo(CYRCLE_CENTER_X, CYRCLE_CENTER_Y);
-		hourHandX = CYRCLE_CENTER_X + hourHandLength - 20;
-		hourHandXLength = CYRCLE_CENTER_X - hourHandX;
-		hourHandYLength = getHourHandYLength(hourHandXLength);
-		hourHandY = CYRCLE_CENTER_Y - hourHandYLength;
-
-		ctx.lineTo(hourHandX, hourHandY);
-		ctx.stroke();
-		
-		//console.log("hourHandLength: " + hourHandLength);
-		//console.log("hourHandX: " + hourHandX);
-		//console.log("hourHandY: " + hourHandY);
-		
-		// minute hand
-		ctx.beginPath();
-		ctx.strokeStyle = 'blue';
-		ctx.lineWidth= '1';
-		ctx.moveTo(CYRCLE_CENTER_X, CYRCLE_CENTER_Y);
-		ctx.lineTo(CYRCLE_CENTER_X, CYRCLE_CENTER_Y + CYRCLE_RADIUS);
-		ctx.stroke();
-		*/
-		
-		min = 59;
-		proc = min * 100 / 60;
-		console.log("proc: " + proc);
-		angle = proc * 360 / 100;
-		console.log("angle: " + angle);
-		hourHandXLength = hourHandLength * Math.sin(Math.PI * angle / 180);
-		console.log(hourHandLength * Math.sin(Math.PI * angle / 180));
-		hourHandYLength = hourHandLength * Math.cos(Math.PI * angle / 180);
-		console.log(hourHandLength * Math.cos(Math.PI * angle / 180));
-		
-		testHand = new Hand(
-			CYRCLE_CENTER_X, 
-			CYRCLE_CENTER_Y, 
-			CYRCLE_CENTER_X + hourHandXLength, 
-			CYRCLE_CENTER_Y - hourHandYLength
-		);
-		testHand.draw(ctx);
+		redraw(ctx);
+		setInterval(function(){
+			redraw(ctx);
+		}, 1000);
 	}
 });
 
@@ -91,4 +48,33 @@ class Hand {
 		context.lineTo(this.endPosX, this.endPosY);
 		context.stroke();
 	}
+}
+
+function drawCircle(context) {
+	context.beginPath();
+	context.strokeStyle = '#000000';
+	// arc(x, y, radius, startAngle, endAngle, anticlockwise)
+	context.arc(CYRCLE_CENTER_X, CYRCLE_CENTER_Y, CYRCLE_RADIUS, 0, 2 * Math.PI);
+	context.stroke();
+}
+
+function redraw(context) {
+	var sec = new Date().getSeconds();
+	proc = sec * 100 / 60;
+	//console.log("proc: " + proc);
+	angle = proc * 360 / 100;
+	//console.log("angle: " + angle);
+	hourHandXLength = hourHandLength * Math.sin(Math.PI * angle / 180);
+	//console.log(hourHandLength * Math.sin(Math.PI * angle / 180));
+	hourHandYLength = hourHandLength * Math.cos(Math.PI * angle / 180);
+	//console.log(hourHandLength * Math.cos(Math.PI * angle / 180));
+	testHand = new Hand(
+		CYRCLE_CENTER_X, 
+		CYRCLE_CENTER_Y, 
+		CYRCLE_CENTER_X + hourHandXLength, 
+		CYRCLE_CENTER_Y - hourHandYLength
+	);
+	context.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+	drawCircle(context);
+	testHand.draw(context);
 }
