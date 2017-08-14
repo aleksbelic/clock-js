54361e04478b4d8097d40cbe923e63de3de1c8b4
Aleksandar <aleks.belic@gmail.com>
2017-08-15 00:37:53 +0200

    All styling in vars, all 3 hands added, trigonometry now in draw method.

--- clockJS.js
+++ clockJS.js
@@ -1,28 +1,38 @@
-var CANVAS_WIDTH = 300;
-var CANVAS_HEIGHT = 300;
-var CYRCLE_CENTER_X = 150;
-var CYRCLE_CENTER_Y = 150;
-var CYRCLE_RADIUS = 100;
-var hourHandLengthProcent = 100;
-var hourHandLength = CYRCLE_RADIUS * hourHandLengthProcent / 100;
-var hourHandX;
-var hourHandXLength;
-var hourHandY;
-var hourHandYLength;
+// canvas
+var canvasSize = 300;
+var canvasBorder = '1px solid #d3d3d3';
+
+// clock face
+var circleRadius = 120;
+var circleCenterX = canvasSize / 2;
+var circleCenterY = canvasSize / 2;
+var circleBorderColor = '#000000';
+var circleBorderWidth = 1;
+
+// clock hands
+var hourHandLengthProcent = 60;
+var hourHandLength = circleRadius * hourHandLengthProcent / 100;
+var hourHandWidth = 2;
+var hourHandColor = 'blue';
+var minHandLengthProcent = 80;
+var minHandLength = circleRadius * minHandLengthProcent / 100;
+var minHandWidth = 2;
+var minHandColor = 'red';
+var secHandLengthProcent = 80;
+var secHandLength = circleRadius * secHandLengthProcent / 100;
+var secHandWidth = 2;
+var secHandColor = 'green';
 
 var hour = new Date().getHours();
-//console.log("hour: " + hour);
 var min = new Date().getMinutes();
-//console.log("min: " + min);
+var sec = new Date().getSeconds();
 
-function getHourHandYLength(hourHandXLength) {
-	return Math.sqrt(hourHandLength * hourHandLength - hourHandXLength * hourHandXLength);
-}
-
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener('DOMContentLoaded', function() {
 	var canvas = document.getElementById('clockJS-canvas');
-	canvas.setAttribute("width", CANVAS_WIDTH);
-	canvas.setAttribute("height", CANVAS_HEIGHT);
+	canvas.setAttribute('width', canvasSize);
+	canvas.setAttribute('height', canvasSize);
+	canvas.style.border = canvasBorder;
+	
 	if (canvas.getContext) {
 		var ctx = canvas.getContext('2d');
 		redraw(ctx);
@@ -33,48 +43,50 @@ document.addEventListener("DOMContentLoaded", function() {
 });
 
 class Hand {
-	constructor(startPosX, startPosY, endPosX, endPosY) {
-		this.startPosX = startPosX;
-		this.startPosY = startPosY;
-		this.endPosX = endPosX;
-		this.endPosY = endPosY;
+	constructor(length, width, color) {
+		this.length = length;
+		this.width = width;
+		this.color = color;
 	}
 	
-	draw(context){
+	draw(context, angle){
+		var handXLength = this.length * Math.sin(Math.PI * angle / 180);
+		var handYLength = this.length * Math.cos(Math.PI * angle / 180);
 		context.beginPath();
-		context.strokeStyle = 'green';
-		context.lineWidth= '1';
-		context.moveTo(this.startPosX, this.startPosY);
-		context.lineTo(this.endPosX, this.endPosY);
+		context.strokeStyle = this.color;
+		context.lineWidth = this.width;
+		context.moveTo(circleCenterX, circleCenterY);
+		context.lineTo(circleCenterX + handXLength, circleCenterY - handYLength);
 		context.stroke();
 	}
 }
 
 function drawCircle(context) {
 	context.beginPath();
-	context.strokeStyle = '#000000';
+	context.strokeStyle = circleBorderColor;
+	context.lineWidth = circleBorderWidth;
 	// arc(x, y, radius, startAngle, endAngle, anticlockwise)
-	context.arc(CYRCLE_CENTER_X, CYRCLE_CENTER_Y, CYRCLE_RADIUS, 0, 2 * Math.PI);
+	context.arc(circleCenterX, circleCenterY, circleRadius, 0, 2 * Math.PI);
 	context.stroke();
 }
 
 function redraw(context) {
-	var sec = new Date().getSeconds();
-	proc = sec * 100 / 60;
-	//console.log("proc: " + proc);
-	angle = proc * 360 / 100;
-	//console.log("angle: " + angle);
-	hourHandXLength = hourHandLength * Math.sin(Math.PI * angle / 180);
-	//console.log(hourHandLength * Math.sin(Math.PI * angle / 180));
-	hourHandYLength = hourHandLength * Math.cos(Math.PI * angle / 180);
-	//console.log(hourHandLength * Math.cos(Math.PI * angle / 180));
-	testHand = new Hand(
-		CYRCLE_CENTER_X, 
-		CYRCLE_CENTER_Y, 
-		CYRCLE_CENTER_X + hourHandXLength, 
-		CYRCLE_CENTER_Y - hourHandYLength
-	);
-	context.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+		
+	secHand = new Hand(secHandLength, secHandWidth, secHandColor);
+	sec = new Date().getSeconds();
+	secAngle = sec * 6; // sec * 100 / 60 * 360 / 100;
+	
+	minHand = new Hand(minHandLength, minHandWidth, minHandColor);
+	min = new Date().getMinutes();
+	minAngle = (min * 60 + sec) / 10; //((min * 60 + sec) * 100 / 3600) * 360 / 100;
+	
+	hourHand = new Hand(hourHandLength, hourHandWidth, hourHandColor);
+	hour = new Date().getHours();
+	hourAngle = (hour * 60 + min) / 10; //((hour * 60 + min) * 100 / 3600) * 360 / 100;
+	
+	context.clearRect(0, 0, canvasSize, canvasSize); // clear canvas
 	drawCircle(context);
-	testHand.draw(context);
+	hourHand.draw(context, hourAngle);
+	minHand.draw(context, minAngle);
+	secHand.draw(context, secAngle);
 }
