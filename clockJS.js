7063a2a96ac6ed310b2095aad452004899448f17
Aleksandar <aleks.belic@gmail.com>
2017-08-15 23:42:12 +0200

    Adding clock face dots, fixing typos.

--- clockJS.js
+++ clockJS.js
@@ -3,23 +3,26 @@ var canvasSize = 300;
 var canvasBorder = '1px solid #d3d3d3';
 
 // clock face
-var circleRadius = 120;
+var circleRadius = 140;
 var circleCenterX = canvasSize / 2;
 var circleCenterY = canvasSize / 2;
 var circleBorderColor = '#000000';
 var circleBorderWidth = 1;
+var dotSize = 1; // px
+var dotDistancePercent = 90;
+var dotColor = '#000000';
 
 // clock hands
-var hourHandLengthProcent = 60;
-var hourHandLength = circleRadius * hourHandLengthProcent / 100;
+var hourHandLengthPercent = 60;
+var hourHandLength = circleRadius * hourHandLengthPercent / 100;
 var hourHandWidth = 2;
 var hourHandColor = 'blue';
-var minHandLengthProcent = 80;
-var minHandLength = circleRadius * minHandLengthProcent / 100;
+var minHandLengthPercent = 80;
+var minHandLength = circleRadius * minHandLengthPercent / 100;
 var minHandWidth = 2;
 var minHandColor = 'red';
-var secHandLengthProcent = 80;
-var secHandLength = circleRadius * secHandLengthProcent / 100;
+var secHandLengthPercent = 80;
+var secHandLength = circleRadius * secHandLengthPercent / 100;
 var secHandWidth = 2;
 var secHandColor = 'green';
 
@@ -70,22 +73,37 @@ function drawCircle(context) {
 	context.stroke();
 }
 
+function drawDots(context) {
+	var dotCenterX = 0;
+	var dotCenterY = 0;
+	context.fillStyle = dotColor;
+	for (var angle = 0; angle < 360; angle += 6) {
+		context.beginPath();
+		dotCenterX = circleCenterX + (circleRadius * dotDistancePercent / 100 * Math.sin(Math.PI * angle / 180));
+		dotCenterY = circleCenterY - (circleRadius * dotDistancePercent / 100 * Math.cos(Math.PI * angle / 180));
+		// context.fillRect(x, y, width, height);
+		context.fillRect(dotCenterX, dotCenterY, (angle % 15 == 0) ? dotSize * 3: dotSize, (angle % 15 == 0) ? dotSize * 3: dotSize);
+		context.stroke();
+	}
+}
+
 function redraw(context) {
-		
 	secHand = new Hand(secHandLength, secHandWidth, secHandColor);
 	sec = new Date().getSeconds();
 	secAngle = sec * 6; // sec * 100 / 60 * 360 / 100;
 	
 	minHand = new Hand(minHandLength, minHandWidth, minHandColor);
 	min = new Date().getMinutes();
-	minAngle = (min * 60 + sec) / 10; //((min * 60 + sec) * 100 / 3600) * 360 / 100;
+	minAngle = (min * 60 + sec) / 10; // ((min * 60 + sec) * 100 / 3600) * 360 / 100;
 	
 	hourHand = new Hand(hourHandLength, hourHandWidth, hourHandColor);
 	hour = new Date().getHours();
-	hourAngle = (hour * 60 + min) / 10; //((hour * 60 + min) * 100 / 3600) * 360 / 100;
+	hourAngle = (hour * 60 + min) / 10; // ((hour * 60 + min) * 100 / 3600) * 360 / 100;
 	
 	context.clearRect(0, 0, canvasSize, canvasSize); // clear canvas
+	
 	drawCircle(context);
+	drawDots(context);
 	hourHand.draw(context, hourAngle);
 	minHand.draw(context, minAngle);
 	secHand.draw(context, secAngle);
